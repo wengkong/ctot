694973504e7a4e11538dcc1b1fa4480ff4ffd01e
wengkong <wengkong.kam@gmail.com>
2016-01-07 17:26:44 +0800

    Polymer iron-ajax sample

---
+++ src/main/java/com/hpe/ctot/controller/ProfileController.java
@@ -0,0 +1,36 @@
+package com.hpe.ctot.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hpe.ctot.model.Employee;
+import com.hpe.ctot.repository.EmployeeRepository;
+import com.hpe.ctot.service.UserDetails;
+
+@EnableAutoConfiguration
+@RequestMapping("/profile")
+@RestController
+public class ProfileController {
+	private static final Logger log = LoggerFactory.getLogger(ProfileController.class);
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@RequestMapping(method = GET)
+	public Employee findOne() {		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		UserDetails user = (UserDetails) auth.getPrincipal();
+		log.info("Find one " + user.getId());
+		Employee employee = employeeRepository.findOne(user.getId());
+		//employeeRepository.isManager(employee);
+		return employee;
+	}
+}
